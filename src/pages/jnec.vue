fb28eb984bfc1a1430f71c41d8c955edd636be38
VedikaBasarkar <vedika.basarkar30@gmail.com>
2022-09-05 16:02:27 +0530

    Update for JNEC

--- src/pages/jnec.vue
+++ src/pages/jnec.vue
@@ -3,14 +3,20 @@
         <div class="px-6 md:px-20">
             <a href="https://jnec-website.ortigan.dev/" target="_blank"><img class="w-full" src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1656905199/vedika-portfolio/projects/Screenshot_2022-07-04_at_8.56.17_AM_kawc61.png" /></a>
             <div class="text-xl md:text-4xl text-dark my-4 font-primary font-bold">Jawaharlal Nehru Engineering College</div>
-            <div class="text-sm md:text-xl font-poppins font-light md:w-3/4 pb-5">
+            <!-- <div class="text-sm md:text-xl font-poppins font-light md:w-3/4 pb-5">
                 Mahatma Gandhi Mission’s Jawaharlal Nehru Engineering College, Aurangabad is a special client of Ortigan’s, because all of Ortigan’s core team hails from that very institute. JNEC had planned to establish an educational university of their own, on par with BAMU and such - a first of its kind in Marathwada. They needed a total technological transformation to go with such a massive leap. Ortigan rebuilt the design schema for JNEC as well as transformed and reformed JNEC’s full technology stack such as to improve the form and functionality of the web presence that JNEC had. The new website was now built using state-of-the-art tech stack that offered better performance at a higher efficiency rate and quicker load times. The performance index of the website was also drastically improved as a result of such a transformation. The new design was put in place to foster a sense of trust within parents and students alike. The reformed design helped tone down the strenuous look and feel of the website prior to the current. Oritgan also developed a CMS for JNEC that featured role-based access control (RBAC). This means, only the members that have the permission to access a module, are given the access to it; and this access is defined by their role in the system rather than a personal choice. For eg: a faculty of a department can only write a blog but not publish it directly. It has to go through the department head for approval. Then, and only then, is the blog published. This feature proved to be incredibly useful for system admins - since, in the previous version of the site, they were the ones to have to compose and publish a blog which they seldom knew anything about. The CMS also simplifies other aspects of admin’s life by providing permissions to appointed HODs to handle the data coming from their respective departments. 
-            </div>
+            </div> -->
             <a class="font-poppins text-primary" href="https://www.figma.com/file/XDunxsAZR48EzkkSi3GTOn/JNEC?node-id=202%3A2" target="_blank">Figma Board Link</a>
-            <img class="container" src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1656862008/vedika-portfolio/projects/JNEC/Existing_website_gp4lxq.png" />
-            <img class="container" src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1656862034/vedika-portfolio/projects/JNEC/dynamic_modules_avzhq9.jpg" />
-            <img class="container" src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1656861991/vedika-portfolio/projects/JNEC/Group_191_uirqum.png" />
-            <img class="container" src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1656862034/vedika-portfolio/projects/JNEC/dynamic_modules_avzhq9.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373546/website/jnec/Slide_16_9_-_1_be5f9v.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373546/website/jnec/Slide_16_9_-_2_rt7uht.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373547/website/jnec/Slide_16_9_-_3_ctrpju.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373546/website/jnec/Slide_16_9_-_4_yacuo4.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373554/website/jnec/Slide_16_9_-_8_f9xnvs.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373547/website/jnec/Slide_16_9_-_7_c9vrrm.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373550/website/jnec/Slide_16_9_-_9_pqxho5.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373546/website/jnec/Slide_16_9_-_6_t0jvzm.jpg" />
+            <a class="font-poppins text-primary my-4" href="https://www.figma.com/file/XDunxsAZR48EzkkSi3GTOn/JNEC?node-id=202%3A2" target="_blank">New Website Link</a>
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662373547/website/jnec/Slide_16_9_-_17_pyyk26.jpg" />
         </div>
     </MainLayout>
 </template>
--- src/pages/keychron.vue
+++ src/pages/keychron.vue
@@ -1,12 +1,12 @@
 <template>
  <MainLayout class="">
     <div class="container">
-        <!-- <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662097175/website/keychron/1_tnszxs.jpg" /> -->
-        <div class="item-group" xyz="fade down-100% back-5 stagger-6">
+        <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662097175/website/keychron/1_tnszxs.jpg" />
+        <!-- <div class="item-group" xyz="fade down-100% back-5 stagger-6">
             <div class="square xyz-in bg-red-300 p-10">hi</div>
             <div class="square xyz-in bg-red-300 p-10">hello</div>
             <div class="square xyz-in bg-red-300 p-10">heyo</div>
-        </div>
+        </div> -->
         <!-- <XyzTransitionGroup
         class="item-group"
         xyz="down-100% back-5 stagger-6"
