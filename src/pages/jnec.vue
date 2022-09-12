81cf18e104af8b62ee297cc6593c408aa50e47e4
VedikaBasarkar <vedika.basarkar30@gmail.com>
2022-09-12 17:27:59 +0530

    Update

--- src/pages/jnec.vue
+++ src/pages/jnec.vue
@@ -3,7 +3,7 @@
         <div class="px-6 md:px-20">
             <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381061/website/jnec/a1_jynfak.jpg" />
             <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381062/website/jnec/a2_zspxyh.jpg" />
-            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381062/website/jnec/a3_jicj6k.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662983725/website/jnec/J3_lxvzpq.jpg" />
             <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381063/website/jnec/a4_redpkz.jpg" />
             <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381062/website/jnec/a5-1_gsvfcg.jpg" />
             <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381071/website/jnec/a5_su8g0a.jpg" />
@@ -14,7 +14,16 @@
             <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662488781/website/jnec/a10_r6urft.jpg" />
             <!-- <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381064/website/jnec/a11_gcm5dn.jpg" /> -->
             <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662489077/website/jnec/a12_giuwbj.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662983729/website/jnec/J9_rbkbke.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662983728/website/jnec/J10_stxiwa.jpg" />
             <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381065/website/jnec/a13_fnlns6.jpg" />
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662983743/website/jnec/J16_cwenyg.jpg" />
+
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662983734/website/jnec/J17-1_dweajc.jpg" />
+
+            <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662983752/website/jnec/J17_o7pa9v.jpg" />
+
+
             <!-- <img src="https://res.cloudinary.com/db3h7h0pa/image/upload/v1662381065/website/jnec/a14_ozr3fg.jpg" /> -->
             <div class="flex flex-row justify-center w-full">
               <g-link class="font-poppins redirectLink text-xl justify-center" to="https://jnec-website.ortigan.dev/" target="_blank">New Website Link</g-link>
